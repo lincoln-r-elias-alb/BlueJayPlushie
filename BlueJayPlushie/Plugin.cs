98c3c83ad0d87576703ec4eb2882e0061aa63947
Lincoln dos Reis Elias <lincolnreistkd@gmail.com>
2024-01-13 19:50:14 -0300

    Initial commit

---
+++ BlueJayPlushie/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using LethalLib.Modules;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace BlueJayPlushie
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static AssetBundle BlueJayAssets;
+        public static ConfigFile config;
+
+        private void Awake()
+        {
+            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            BlueJayAssets = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "bluejaymod"));
+            if (BlueJayAssets == null)
+            {
+                Logger.LogError("Failed to load custom assets.");
+                return;
+            }
+            int iRarity = 100;
+            Item blueJayPlushie = BlueJayAssets.LoadAsset<Item>("Assets/Bluejayplushie.asset");
+            Utilities.FixMixerGroups(blueJayPlushie.spawnPrefab);
+            NetworkPrefabs.RegisterNetworkPrefab(blueJayPlushie.spawnPrefab);
+            Items.RegisterScrap(blueJayPlushie, iRarity, Levels.LevelTypes.All);
+            new Harmony("BlueJayPlushie").PatchAll();
+            Logger.LogMessage("Blue Jay Plushie Mod loaded!");
+
+        }
+    }
+}
---
+++ BlueJayPlushie/obj/Release/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace BlueJayPlushie
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "BlueJayPlushie";
+        public const string PLUGIN_NAME = "BlueJayPlushie";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
